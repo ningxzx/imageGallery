ff3444a40bcafe537a3bf33b73b661ca8264445e
ningxzx <370736274@qq.com>
2018-11-22 11:58:51 +0800

    fix bug,Need add phantom.js

--- components/index.js
+++
@@ -1,8 +0,0 @@
-import Perspective from './Perspective.vue'
-
-function components(Vue) {
-    Vue.component('Perspective', Perspective)
-}
-export default () => {
-  Vue.use(components)
-}
--- nuxt.config.js
+++ nuxt.config.js
@@ -38,8 +38,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/element-ui',
-    '@/components/index'
+    '@/plugins/element-ui'
   ],
 
   /*
