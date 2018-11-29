104c87e75427226cad3592b4e24b73f631df9879
ningxzx <370736274@qq.com>
2018-11-29 16:26:09 +0800

    wtf

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@
       title="上传图片"
       :visible.sync="centerDialogVisible"
       width="30%">
-      <el-upload class="upload-demo"  action="/api/uploadPic"  :limit="5" drag multiple :http-request="upload">
+      <el-upload class="upload-demo"  :limit="5" drag multiple :http-request="upload">
         <i class="el-icon-upload"></i>
         <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
         <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
@@ -65,7 +65,7 @@ export default {
     Perspective
   },
   async asyncData({ app }) {
-    const res = await app.$axios.$get("/images");
+    const res = await app.$axios.$get("/api/images");
     return {
       images: res.data.map(img => {
         return {
@@ -85,7 +85,7 @@ export default {
   },
   methods: {
     async getImages() {
-      const res = await this.$axios.$get("/images");
+      const res = await this.$axios.$get("/api/images");
       this.images = res.data.map(img => {
         return {
           src: img,
@@ -103,7 +103,7 @@ export default {
         .then(function(compressedFile) {
           var form = new FormData();
           form.append("file", compressedFile);
-          _this.$axios.post("/uploadPic", form).then(_this.getImages);
+          _this.$axios.post("/api/uploadPic", form).then(_this.getImages);
         })
         .catch(function(error) {
           console.log(error.message);
